a2166f51e99031c0cf864ed7ab7610ae35991847
Loafter <mopper@yandex.ru>
2015-07-17 22:03:35 +0300

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This autorun writen by Andrew Rose GE HC Russia")
+	cmd := exec.Command("java", "-jar","DICOMVWR.CAB")
+	err := cmd.Start()
+	if err != nil {
+		down:=exec.Command("explorer","https://java.com/ru/download/")
+		down.Start()
+		log.Fatal(err)
+	}
+
+}
